e042b570f80c708505b16dc475d7ed231cb85f24
esakkirajmari <esakkirajmari@gmail.com>
2022-12-20 22:30:08 +0530

    index.html

---
+++ js/main.js
@@ -0,0 +1,49 @@
+let car={
+    carname:"tata",
+    color:"blue",
+    budget:"under 4 lakh",
+    bodytype:"suv",
+    fueltype:"petrol",
+    transmission:"automatic",
+    seatingcapacity:"5seater",
+}
+console.log(car);
+
+let fruits={
+    apple:"redcolor",
+    orange:"orangrcolor",
+    banana:"yellow",
+    grapes:"black",
+    mango:"yellow",
+}
+console.log(fruits)
+
+let bike={
+    bikename:"x-blade",
+    enginetype:"Bs6",
+    gearbox:"5speed",
+    fueltype:"petrol",
+    headlamp:"LED",
+    tyretype:"Tubeless",
+}
+console.log(bike)
+
+let phone={
+    name:"realme-5s",
+    network:"4g",
+    launch:"2019,november 20",
+    weight:"198 g",
+    sim:"dual sim",
+    price:"10000",
+}
+console.log(phone)
+
+let suryamovies={
+    1997:"Nerukku Ner",
+    2009:"Ayan",
+    2019:"Kaapaan",
+    2020:"Soorari Pottru",
+    2021:"jai bhim",
+    2022:"Etharkkum Thunindhavan",
+}
+console.log(suryamovies)
